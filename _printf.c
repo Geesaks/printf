c00d99c4011152e0a4f39c17da6ee1ef19b64709
Gideon Ayo <geesaks007@gmail.com>
2023-04-18 22:30:01 +0100

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -9,13 +9,13 @@ void print_buffer(char buffer[], int *buff_ind);
  */
 int _printf(const char *format, ...)
 {
-	int i, printed = 1, printed_chars = 1;
-	int flags, width, precision, size, buff_ind = 0;
+	int i, printed = 0, printed_chars = 0;
+	int flags, widness, precision, size, buff_ind = 0;
 	va_list list;
 	char buffer[BUFF_SIZE];
 
 	if (format == NULL)
-		return (0);
+		return (-1);
 
 	va_start(list, format);
 
